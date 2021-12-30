151dbb96cf32a4ee0b9f76c6343a7360685c4eaf
Ryan Furrer <rfurrertech@gmail.com>
2021-12-29 19:15:06 -0500

    Flesh out header

--- js/theme-change.js
+++ js/theme-change.js
@@ -1,16 +1,19 @@
 function changeTheme() {
   let html = document.querySelector('HTML');
   let btn = document.getElementById('theme-change');
+  let headerImage = document.getElementById('header-image');
 
   btn.addEventListener('click', () => {
     if (html.className === 'light') {
       html.classList.add('dark');
       btn.classList.remove('fa-sun');
       btn.classList.add('fa-moon');
+      headerImage.src = '/images/header-dark.svg';
     } else {
       html.classList.remove('dark');
       btn.classList.remove('fa-moon');
       btn.classList.add('fa-sun');
+      headerImage.src = '/images/header-light.svg';
     }
   });
 }
