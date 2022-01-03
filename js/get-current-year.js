4e55cbcc222f9224f870700c3f4bcbe1bf251c09
Ryan Furrer <rfurrertech@gmail.com>
2022-01-03 13:07:20 -0500

    Completed Projects for mobile and smaller viewports

--- js/get-current-year.js
+++ js/get-current-year.js
@@ -1,6 +1,6 @@
 function getCurrentYear() {
   let footerYear = document.querySelector('#footer-year'); // Grab span element containing year in footer
-  const date = new Date('01/20/2021'); // Gets current date
+  const date = new Date(); // Gets current date
   const year = date.getFullYear().toString(); // Gets current year from date and converts it to a string
 
   footerYear.innerHTML = year; // Changes year in footer
---
+++ js/project-tab.js
@@ -0,0 +1,26 @@
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(event) {
+  // hide all tab panels
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+  // mark all tabs as unselected
+  tabButtons.forEach((tab) => {
+    tab.ariaSelected = false;
+    tab.setAttribute('aria-selected', false);
+  });
+
+  event.currentTarget.setAttribute('aria-selected', true);
+
+  const id = event.currentTarget.id;
+  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  // console.log(tabPanel);
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener('click', handleTabClick)
+);
