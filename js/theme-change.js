54e65b57cd8a2da3b160cfbacdd84a4d0f4bd1ea
Ryan Furrer <rfurrertech@gmail.com>
2022-01-15 11:03:03 -0500

    Fixed homepage header image
    Previous change from #header-image to .header-image for reusability broke the function to change the header-image on the homepage - this is the only image that is theme-dependent.
    
    Added new ID to the homepage header-image and re-linked JavaScript to it.
    
    Also renamed images for easier understanding.

--- js/theme-change.js
+++ js/theme-change.js
@@ -1,19 +1,19 @@
 function changeTheme() {
   let html = document.querySelector('HTML');
   let btn = document.getElementById('theme-change');
-  let headerImage = document.getElementById('header-image');
+  let headerImage = document.getElementById('home-header-image');
 
   btn.addEventListener('click', () => {
     if (html.className === 'light') {
       html.classList.add('dark');
       btn.classList.remove('ph-sun');
       btn.classList.add('ph-moon');
-      headerImage.src = '/images/portfolio-image-light.png';
+      headerImage.src = '/images/portfolio-image-dark-theme.png';
     } else {
       html.classList.remove('dark');
       btn.classList.remove('ph-moon');
       btn.classList.add('ph-sun');
-      headerImage.src = '/images/portfolio-image-dark.png';
+      headerImage.src = '/images/portfolio-image-light-theme.png';
     }
   });
 }
