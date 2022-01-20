90817e89e5163186d8dad7c0527811a4520978cb
Ryan Furrer <rfurrertech@gmail.com>
2022-01-19 19:33:54 -0500

    Reduced image file sizes and converted to .webp
    In an effort to reduce data transfer and increase performance.

--- js/theme-change.js
+++ js/theme-change.js
@@ -8,12 +8,12 @@ function changeTheme() {
       html.classList.add('dark');
       btn.classList.remove('fa-sun');
       btn.classList.add('fa-moon');
-      headerImage.src = '/images/portfolio-image-dark-theme.png';
+      headerImage.src = '/images/portfolio-image-dark-theme.webp';
     } else {
       html.classList.remove('dark');
       btn.classList.remove('fa-moon');
       btn.classList.add('fa-sun');
-      headerImage.src = '/images/portfolio-image-light-theme.png';
+      headerImage.src = '/images/portfolio-image-light-theme.webp';
     }
   });
 }
