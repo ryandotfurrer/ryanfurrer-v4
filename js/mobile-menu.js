a9519e0d9f94768809a6407d7882b1facef84eec
Ryan Furrer <rfurrertech@gmail.com>
2022-07-07 17:01:11 -0400

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,24 +1,24 @@
 function mobileMenu() {
-  const hamburger = document.querySelector('#hamburger');
-  const navList = document.querySelector('.nav-list');
+	const hamburger = document.querySelector("#hamburger");
+	const navList = document.querySelector(".nav-list");
 
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navList.classList.toggle('active');
-  });
+	hamburger.addEventListener("click", () => {
+		hamburger.classList.toggle("active");
+		navList.classList.toggle("active");
+	});
 }
 
 function closeMenu() {
-  const hamburger = document.querySelector('#hamburger');
-  const navList = document.querySelector('.nav-list');
-  const navLink = document.querySelectorAll('.nav-link');
+	const hamburger = document.querySelector("#hamburger");
+	const navList = document.querySelector(".nav-list");
+	const navLink = document.querySelectorAll(".nav-link");
 
-  navLink.forEach((n) =>
-    n.addEventListener('click', () => {
-      hamburger.classList.remove('active');
-      navList.classList.remove('active');
-    })
-  );
+	navLink.forEach((n) =>
+		n.addEventListener("click", () => {
+			hamburger.classList.remove("active");
+			navList.classList.remove("active");
+		})
+	);
 }
 
 closeMenu();
